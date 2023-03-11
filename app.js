c809eb09b48a36e065a0a73ca419dbec1faf7972
Hung Bui <hungbui7690@gmail.com>
2023-03-11 08:03:53 -0700

    Throw an Error with a Simple Test in JavaScript

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 /*
-
+  Throw an Error with a Simple Test in JavaScript 
 */
+
+const sum = (a, b) => a - b // this logic is wrong
+const subtract = (a, b) => a - b
+
+/////////////////////////
+// TEST
+/////////////////////////
+let result = sum(3, 7)
+let expected = 10
+
+if (result !== expected)
+  throw new Error(`${result} is not equal to ${expected}`)

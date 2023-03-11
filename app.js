9ebb0e920482fea48f2a9b66cdbbc6d22ab34c31
Hung Bui <hungbui7690@gmail.com>
2023-03-11 08:09:34 -0700

    Abstract Test Assertions into a JavaScript Assertion Library

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 /*
-  Throw an Error with a Simple Test in JavaScript 
+  Abstract Test Assertions into a JavaScript Assertion Library
+  - we try to simulate expect function of testing library
 */
 
-const sum = (a, b) => a - b // this logic is wrong
-const subtract = (a, b) => a - b
+const { sum, subtract } = require('./math')
 
-/////////////////////////
-// TEST
-/////////////////////////
-let result = sum(3, 7)
-let expected = 10
+let result, expected
 
-if (result !== expected)
-  throw new Error(`${result} is not equal to ${expected}`)
+result = sum(3, 7)
+expected = 10
+
+expect(result).toBe(expected) // (2a)
+
+/////////////////////////////////
+
+result = subtract(3, 7)
+expected = -1
+expect(result).toBe(expected) // (2b) right now, we cannot see the result of this since 2a throws error
+
+/////////////////////////////////
+
+// (1)
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`)
+      }
+    },
+  }
+}
